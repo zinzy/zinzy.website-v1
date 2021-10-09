1b8d35a845de30d16cf766e21bcda11e5f2d58f1
Zinzy Nev Geene <zinzygeene@gmail.com>
2021-10-09 11:52:32 +0200

    Revert "Grid"
    This reverts commit 0c7af86333a2f06d138f0458c6cf8e56b9487263.

--- _writing/2021-10-03-sharpening-pencils/breakoutcouples.jsx
+++ _writing/2021-10-03-sharpening-pencils/breakoutcouples.jsx
@@ -7,10 +7,10 @@ export default function BreakoutCouples() {
         <div className="breakout">
             <div className="breakout-content mail">
                 <div className="row g-0">
-                    <div className="col-12 col-md-6">
+                    <div className="col-6">
                         <img src="https://res.cloudinary.com/dbi2zounq/image/upload/v1633156633/journal/joe_mvaesx.png" alt="" className="img-fluid"/>
                     </div>
-                    <div className="col-12 col-md-6">
+                    <div className="col-6">
                         <img src="https://res.cloudinary.com/dbi2zounq/image/upload/v1633156878/journal/kathleen_brjras.png" alt="" className="img-fluid"/>
                     </div>
                 </div>
