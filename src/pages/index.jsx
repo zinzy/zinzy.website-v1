280405633ede99dd294dabf985e6b0b4e523c5d7
Zinzy Nev Geene <zinzygeene@gmail.com>
2022-02-05 07:12:18 +0100

    Update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,43 +8,31 @@ import Layout from "../components/layout"
 const IndexPage = ({data}) => { 
   return (
     <Layout>  
-      <header id="header">
-            <h1 className="mb-1 p-0">Zinzy Nev Geene</h1> 
-            <ul className="list-unstyled">
-              <li>Design & Product Manager</li>
-              <li>Queer Catholic facilitator</li>
-              <li>Last updated on {data.site.buildTime}</li>
-            </ul>
-      </header> 
 
-      <section>
-        <h2>Welcome</h2>
-        <p>I'm a designer with 15 years of experience shipping products and leading design as well as product teams. I am specialized in making tools for people at work. My work is focused on organizing skill sets and relationships to demonstrate how caring for users is good for business.</p>
-        <p>At present, I serve as Design & Product Manager of <a href="https://www.leeruniek.nl" target="_blank" rel="noopener noreferrer">Leeruniek</a>, a scale-up in education technology. Previously, I spent five years in travel technology at <a href="https://www.airtrade.com" target="_blank" rel="noopener noreferrer">Airtrade</a>, where I led research and design on enterprise and consumer platforms. Before that, I ran Studio Zinzy for eight years, offering design and development across academia, journalism, health care, and other industries.</p>
-        <p>Unprofessionally, I create resonant queer spaces. I'm on the leadership team of <a href="https://www.vineandfig.co" target="_blank" rel="noopener noreferrer">Vine & Fig</a>, an international organization for LGBTQIA+ Catholics. Every last Sunday of the month when Covid doesn't restrict intimate gatherings, I host <a href="https://www.queersalon.us" target="_blank" rel="noopener noreferrer">Queer Salon</a> in Amsterdam. I take pleasure in cultivating environments that are intersectional, curious, and kind.</p>
-        <p>You may find me on the Amsterdam East side tending to the home and garden I share with <a href="https://twitter.com/anjawaleson" target="_blank" rel="noopener noreferrer">Anja</a>. I enjoy making music, learning the love language of cooking, and exploring the worlds of theology, sociolinguistics, and psychology. I'm a fan of Barbra Streisand, monster movies, and vigorous note taking.</p>
-        <p>Life is truly good.</p>
-      </section>
-
-      <section className="border-bottom small text-muted">
-        <ul className="list-inline">
-          <li className="list-inline-item">Available on:</li>
-          <li className="list-inline-item"><a href="mailto:zinzy@pm.me" rel="me">Email</a></li>
-          <li className="list-inline-item"><a href="https://www.linkedin.com/in/zinzy/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+      <header>
+        <ul class="list-inline">
+          <li class="list-inline-item"><h1>Zinzy Nev Geene</h1></li>
+          <li class="list-inline-item">Amsterdam</li>
         </ul>
-      </section> 
+
+        <h2>I manage product <span className="br"></span> and design.</h2>
+      </header>
 
       <section>
-        <h2>My work</h2>
-        <div className="mt-5 ">
+        <p>I have <mark>15 years of experience</mark> shipping products and leading design and product teams. My work has helped me hone my craft in design, research, strategy, and leadership. These days, I organize skill sets and relationships to demonstrate how <mark>caring for users is good for business</mark>.</p>
+      </section>
+      
+      <section>
+        <h3>How I work</h3>
+        <div className="mt-5">
           <div className="d-md-flex align-items-center">
             <div>
-            <div className="tile tile-yellow mb-4 mb-md-0">🧑‍🏫</div>
+            <div className="tile tile-moss mb-4 mb-md-0">🧑‍🏫</div>
             </div>
             <div className="flex-grow-1 align-items-center">
               <div>
-              <h3 className="h6 mb-1">Rooting for teachers</h3>
-              <p className="m-0 p-0">I build tools that help people solve problems at work. At Leeruniek, we cultivate collaborative skills in learning analytics in Dutch primary schools so that pupils can thrive.</p>
+              <h4 className="mb-1">Serving the working crowd</h4>
+              <p className="m-0 p-0">I build tools that help people solve problems at work. I appreciate the complexity, challenges, and longevity of enterprise product development.</p>
               </div>
             </div> 
           </div>
@@ -56,8 +44,8 @@ const IndexPage = ({data}) => {
             </div>
             <div className="flex-grow-1 align-items-center">
               <div>
-              <h3 className="h6 mb-1">Strategy and management</h3>
-              <p className="m-0 p-0">My team works to gain a deep understanding of users, and aligns with business and technology to define challenges. I turn big dreams into snackable chunks, and mentor along the way.</p>
+              <h4 className="mb-1">Strategy and leadership</h4>
+              <p className="m-0 p-0">My teams gain deep and sustainable insight into users. Meanwhile, I turn big dreams into snackable chunks, and mentor along the way.</p>
               </div>
             </div> 
           </div>
@@ -69,8 +57,8 @@ const IndexPage = ({data}) => {
             </div>
             <div className="flex-grow-1 align-items-center">
               <div>
-              <h3 className="h6 mb-1">DesignOps and facilitation</h3>
-              <p className="m-0 p-0">I give designers and researchers methods and tools to collaborate well across the company, so that they can focus on what they do best: make an impact.</p>
+              <h4 className="mb-1">Ops and facilitation</h4>
+              <p className="m-0 p-0">I give designers and researchers tools to collaborate well across the company, so that they can focus on what they do best: make an impact.</p>
               </div>
             </div> 
           </div>
@@ -78,74 +66,28 @@ const IndexPage = ({data}) => {
       </section>
 
       <section>
-        <h2>My volunteering</h2>
-        <div className="mt-5 ">
-          <div className="d-md-flex align-items-center">
-            <div>
-            <div className="tile tile-green mb-4 mb-md-0">🪴</div>
-            </div>
-            <div className="flex-grow-1 align-items-center">
-              <div>
-              <h3 className="h6 mb-1">Cultivating communities</h3>
-              <p className="m-0 p-0">I bring people together on the Internet and in Amsterdam to create intersectional communities that help people connect, recharge, and learn from one another.</p>
-              </div>
-            </div> 
-          </div>
-       </div>
-        <div className="mt-5 ">
-          <div className="d-md-flex align-items-center">
-            <div>
-            <div className="tile tile-brown mb-4 mb-md-0">🍩</div>
-            </div>
-            <div className="flex-grow-1 align-items-center">
-              <div>
-              <h3 className="h6 mb-1">Facilitating gatherings</h3>
-              <p className="m-0 p-0">I host Reading Clubs, prayer sessions, panel discussions, holiday marathons, and conversations that thrive on lived experiences, vulnerability, and generous assumptions.</p>
-              </div>
-            </div> 
-          </div>
-       </div>
-        <div className="mt-5 ">
-          <div className="d-md-flex align-items-center">
-            <div>
-            <div className="tile tile-yellow mb-4 mb-md-0">📣</div>
-            </div>
-            <div className="flex-grow-1 align-items-center">
-              <div>
-              <h3 className="h6 mb-1">Writing and speaking</h3>
-              <p className="m-0 p-0">I write Scripture reflections, poetry, and essays, produce podcasts series, and speak about what it means to be a queer Afropean Catholic embracing the uncomfortable.</p>
-              </div>
-            </div> 
-          </div>
-       </div>
-      </section>  
+        <h3>My experience</h3>
+        <p>At present, I serve as Design & Product Manager of <a href="https://www.leeruniek.nl" target="_blank" rel="noopener noreferrer">Leeruniek</a>, a scale-up in education technology. We cultivate better collaborative and analytical skills in teams of primary school teachers. <mark>Improving the lives of crucial workers</mark> by giving them the right tools and conversations is indeed very fulfilling.</p>
+        <p>Previously, I spent five years at <a href="https://www.airtrade.com" target="_blank" rel="noopener noreferrer">Airtrade</a>, where I led research and design on a dozen enterprise and consumer platforms for KLM and other leaders in the travel industry. Before that, I ran Studio Zinzy for eight years, providing design and development services in health care, journalism, fine arts, academia, and other spaces.</p>
+      </section>
 
       <section>
-        <h2>Availability</h2>
-        <p>At the time of writing, I am not available for design or research efforts. I do, however, have  limited availability to take on projects that require design, research or product consulting. Shoot me an <a href="mailto:zinzy@pm.me">email</a> to tell me about your project.</p>
+        <h3>Life is good</h3>
+      <p>You may find me on the Amsterdam East side tending to the home and garden I share with <a href="https://twitter.com/anjawaleson" target="_blank" rel="noopener noreferrer">Anja</a>. I enjoy making music, learning the love language of cooking, and exploring the worlds of theology, sociolinguistics, and psychology. I'm a fan of Barbra Streisand, monster movies, and terribly television. Life is truly good.</p>
       </section>
+
+      <section>
+        <h3>Availability</h3>
+      <p>Aside from my work at Leeruniek, I sometimes engage in consulting on matters of product and design. At present, I do not have availability for this. Feel free to reach out to me, though, about your project or any other topic. I enjoy meeting new people and their stories. You can find me on <a href="mailto:zinzy@pm.me">email</a> and <a href="https://www.linkedin.com/in/zinzy/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.</p>
+      </section>
+
+
+ 
 
       <footer className="small text-muted">
-        <p>This one-pager is built using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a>, hands, and infinite appreciation for simplicity, the <a href="https://indieweb.org/" target="_blank" rel="noopener noreferrer">IndieWeb</a>, as well as the quieter days of personal sites.</p>
+        <p>This space is built using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a>, hands, and infinite appreciation for simplicity, the <a href="https://indieweb.org/" target="_blank" rel="noopener noreferrer">IndieWeb</a>, as well as the quieter days of personal sites. It was last updated on {data.site.buildTime}.</p>
       </footer>
 
-
-{/* 
-      <p>Hello, I'm Zinzy, a designer with 13 years of skin in the game. I deploy my multidisciplinary skillset in leadership, design thinking, and operations to help teams do better. I'm specialized in building tools that help people solve problems at work. At the moment I lead product and design at edtech startup <a href="https://www.leeruniek.nl" target="_blank" rel="noopener noreferrer">Leeruniek</a>. Literally couldn't wish for better colleagues.</p>
-      <p>Outside of work I build resonant queer spaces. I'm on the leadership team of <a href="https://www.vineandfig.co" target="_blank" rel="noopener noreferrer">Vine & Fig</a>, an international organization for queer Catholics. Every last Sunday of the month I host <a href="https://www.queersalon.us" target="_blank" rel="noopener noreferrer">Queer Salon</a> in Amsterdam.</p>
-      <p>You may find me on the Amsterdam East side tending to the home and garden I share with <a href="https://twitter.com/anjawaleson" target="_blank" rel="noopener noreferrer">Anja</a>. I enjoy writing and making music, learning the love language of cooking, and discovering new wonders in the world of theology, sociolinguistics, and intersectionality. Above all, I'm a fan of Barbra Streisand, garbage television, and mediocre cinema.</p>
-      <p>If you'd like to reach out to me, you can do so via <a href="mailto:zinzy@pm.me">email</a>.</p>
-       <p>This here is my personal site: a collection of soft stances, lived experiences, and critical notes on the things I hold dear. It's built as a scavenger hunt of sorts, mildly awkward on purpose. Its contents are subject to change. A good place to start is my <a href="/Now">[[Now]]</a> page.</p> */}
-      
-      {/* <ul>
-      {
-        data.allMdx.nodes.map(node => (
-          <li key={node.slug}>
-            <a href={node.slug}>{node.frontmatter.title}</a>
-          </li>
-        ))
-      }
-      </ul> */}
     </Layout>
   )
 }
