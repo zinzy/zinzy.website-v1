7442ff87be7d47a2010f074e63184f3062ff8ce4
Zinzy Nev Geene <zinzygeene@gmail.com>
2022-04-06 08:18:06 +0200

    Removed TOC plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,25 +107,6 @@ module.exports = {
               titleToURL: require(`${__dirname}/src/utils/make-slug.js`)
             }
           },
-          {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-              plugins: [
-                {
-                  resolve: `gatsby-remark-table-of-contents`,
-                  options: {
-                    exclude: "Table of Contents",
-                    tight: false,
-                    ordered: false,
-                    fromHeading: 1,
-                    toHeading: 6,
-                    className: "table-of-contents"
-                  },
-                },
-                `gatsby-remark-autolink-headers`
-              ],
-            },
-          },
         ],
       },
     },
