42a179360330e0f72b8a3ac8fb33711c9474b657
Zinzy Nev Geene <zinzygeene@gmail.com>
2022-04-06 18:44:15 +0200

    Removed TIL

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({ children, title, type }) {
       <main>
       <div className="container">
         <div className="row">
-          <div className="col-12 col-md-8 offset-md-2"> 
+          <div className="col-12 col-lg-8 offset-lg-2"> 
             <section className="section">
               <div className="columns is-centered">{children}</div>
             </section> 
