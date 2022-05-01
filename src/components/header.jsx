b505a80f0281b9a5e6ff8e1cc29c62f1c45c84d6
Zinzy Nev Geene <zinzygeene@gmail.com>
2022-05-01 11:23:51 +0200

    Updated micrpub endpoint

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -20,10 +20,11 @@ export default function Header({ title }) {
           {(siteConfig.siteMetadata.title || "Zinzy Waleson") +
             (title ? ` : ${title}` : "")}
         </title>
-
+ 
+        {/* Micropub  */}
         <link rel="authorization_endpoint" href="https://indieauth.com/auth"/>
         <link rel="token_endpoint" href="https://tokens.indieauth.com/token"/>
-        <link rel="micropub" href="https://www.zinzy.website/micropub"/>
+        <link rel="micropub" href="https://zinzy-indieauth.herokuapp.com/micropub" />
  
       </Helmet>  
 
