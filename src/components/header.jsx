016d69041653a797f619b1440674929e5b7e7ac6
Zinzy Nev Geene <zinzygeene@gmail.com>
2022-05-04 18:43:30 +0200

    Updated 404

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,8 +23,7 @@ export default function Header({ title }) {
 
         <link rel="authorization_endpoint" href="https://indieauth.com/auth"/>
 
-        {/* <link rel="micropub" href="https://zinzy.website/micropub/main"/> */}
-        <link rel="micropub" href="https://zinzys-micropub-endpoint.herokuapp.com/"/>
+        <link rel="micropub" href="https://zinzy.website/micropub/main"/>
         <link rel="token_endpoint" href="https://tokens.indieauth.com/token"/>
 
       </Helmet>  
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -11,6 +11,7 @@ const ErrorPage = ({data}) => {
       <section>
         <h2>You've lost your way</h2> 
         <p>Whatever you were looking for is not or no longer here. Use the menu below to find your way to something entertaining. Off you go!</p>
+        <a href="/" className="btn">Go back home</a>
       </section>
     </Layout>
   )
