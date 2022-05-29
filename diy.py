cf4c82d30f10b8cd928d2dd5b67625e6fdd17d93
Zinzy Nev Geene <zinzygeene@gmail.com>
2022-05-29 13:29:56 +0200

    Updated diy.py

--- diy.py
+++ diy.py
@@ -2,14 +2,14 @@
 import os
 import glob
 import markdown
-import frontmatter
+# import frontmatter
 
 if not os.path.exists('public'):
     os.mkdir('public')
 
 for f in glob.iglob('content/*.md'):
-  
     with open(f, 'r') as file:
+        # post = frontmatter.loads(file.read())
         raw = file.read()
         html = markdown.markdown(raw)
 
@@ -18,71 +18,70 @@ for f in glob.iglob('content/*.md'):
 
     with open(destination, 'w') as file:
         file.write(r'''<!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <meta name="author" content="Zinzy Waleson">
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Zinzy Waleson">
 
-          <link rel="canonical" href="https://zinzy.website">
+  <link rel="canonical" href="https://zinzy.website">
 
-          <!-- Primary Meta Tags -->
-          <title>Zinzy Waleson</title>
-          <meta name="title" content="Zinzy Waleson">
-          <meta name="description" content="Zinzy Waleson’s Personal Website">
-          <meta name="author" content="Zinzy Waleson">
-          <link rel="canonical" href="https://zinzy.website/"> 
+  <!-- Primary Meta Tags -->
+  <title>Zinzy Waleson</title>
+  <meta name="title" content="Zinzy Waleson">
+  <meta name="description" content="Zinzy Waleson’s Personal Website">
+  <meta name="author" content="Zinzy Waleson">
+  <link rel="canonical" href="https://zinzy.website/"> 
 
-                    <style>
-                    body {
-                        padding: 20px;
-                        line-height: 150%;
-                      }
-                      @media screen and (min-width: 768px) {
-                        body {
-                          max-width: 600px;
-                          margin: 100px auto; 
-                        }
-                      }
-                      @media screen and (min-width: 992px) {
-                        body {
-                          max-width: 600px;
-                          margin: 200px auto; 
-                        }
-                      }
-                      section {
-                        margin-top: 50px;
-                      }
-                      article {
-                        margin-top: 30px;
-                      }
-                      footer {
-                        font-size: 90%;
-                        margin-top: 100px;
-                      }
-                      a {
-                        color: black;
-                        text-decoration: none;
-                        border-bottom: 1px solid rgb(84, 43, 255);
-                      }
-                      a:hover {
-                        color: deeppink;
-                      }
-                      a:visited {
-                        color: black;
-                      }
-                      a:visited:hover {
-                        color: deeppink;
-                      }</style>
-        </head>
-        <body>
-        ''')
+            <style>
+            body {
+                padding: 20px;
+              }
+              @media screen and (min-width: 768px) {
+                body {
+                  max-width: 600px;
+                  margin: 100px auto; 
+                }
+              }
+              @media screen and (min-width: 992px) {
+                body {
+                  max-width: 600px;
+                  margin: 200px auto; 
+                }
+              }
+              section {
+                margin-top: 50px;
+              }
+              article {
+                margin-top: 30px;
+              }
+              footer {
+                font-size: 90%;
+                margin-top: 100px;
+              }
+              a {
+                color: black;
+                text-decoration: none;
+                border-bottom: 1px solid rgb(84, 43, 255);
+              }
+              a:hover {
+                color: deeppink;
+              }
+              a:visited {
+                color: black;
+              }
+              a:visited:hover {
+                color: deeppink;
+              }</style>
+</head>
+<body>
+''')
         file.write(html)
         file.write(r'''
 
   <footer>
-    <a href="/">Zinzy.website</a> is the personal website of Zinzy Waleson. All the content on this site is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a>
+    Zinzy.website is the personal website of Zinzy Waleson. All the content on this site is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a>
   </footer>
 </body>
 </html>''')
